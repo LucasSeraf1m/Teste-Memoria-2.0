23d7a415c2265f5e87ee8a2c0f368a0a97672d41
vunicius <vinilopesilva1@unesc.net>
2023-07-09 15:48:30 -0300

    estilização login e cadastro

--- src/Components/CadastroUsuario.component.js
+++ src/Components/CadastroUsuario.component.js
@@ -1,10 +1,16 @@
 import React, {useState} from "react"
 import axios from "axios";
+import { useNavigate} from "react-router-dom"
+import styles from './LoginCadastroUsuario.module.css'
+
+
 // import { Link, NavLink } from "react-router-dom";
 
 export default function CadastrarUsuario(){
     const [usuario, setUsuario] = useState('');
     const [senha, setSenha] = useState('');
+    const [islogin,setIsLogin]=useState(false)
+    const navigate = useNavigate();
 
     // const [error, setError] = useState('');
 
@@ -13,6 +19,13 @@ export default function CadastrarUsuario(){
         e.preventDefault();
         try{
             const response = await axios.post('http://localhost:1000/api/cadastro', { usuario, senha });
+            setIsLogin(true)  
+
+            navigate('/login')
+            
+            if(islogin){
+                return navigate("/login",{replace:true})
+            }
             setUsuario('');
             setSenha('');
           
@@ -25,8 +38,9 @@ export default function CadastrarUsuario(){
 
     return(
 
-        <div>
+        <div className={styles.contentLogin}>
 <form onSubmit={handleSubmit}>
+<h2>Cadastro</h2>
 
 <input placeholder="Usuario" value={usuario} onChange={(e)=>setUsuario(e.target.value)}  type="text"></input>
 <input placeholder="Senha" value={senha} onChange={(e)=>setSenha(e.target.value)}  type="password"></input>
--- src/Components/LoginUsuario.component.js
+++ src/Components/LoginUsuario.component.js
@@ -1,6 +1,7 @@
 import React,{useState} from "react"
 import { useNavigate} from "react-router-dom"
 import axios from "axios"
+import styles from './LoginCadastroUsuario.module.css'
 
 function LoginUsuario(){
     const [usuario,setUsuario]=useState('')
@@ -20,14 +21,19 @@ if(islogin){
     return navigate("/home",{replace:true})
 }
   
+setUsuario('');
+setSenha('');
+
  
 }
 catch(error){
     console.log(error)
 }
  }  
-    return(<div>
+    return(<div className={styles.contentLogin}>
     <form onSubmit={hadleSubmit}>
+    <h2>Login</h2>
+
         <input type="text" placeholder="Usuario" onChange={(e)=>setUsuario(e.target.value)}></input>
         <input type="text" placeholder="Senha" onChange={(e)=>setSenha(e.target.value)}></input>
         <button type="submit">Login</button>
